33c527c4f2a31ac0fbb0e6e8726c317113c752df
bmacnaughton <bmacnaughton@gmail.com>
2023-04-24 17:52:19 -0700

    with volumes

--- docker-api.js
+++ docker-api.js
@@ -2,22 +2,71 @@
 
 // candidate to be replaced
 const dockerRawStream = require('docker-raw-stream');
+const debug = require('debug')('docker-run');
+
 
 const SocketConnection = require('./socket-fetch');
 
-class DockerDaemon {
-  constructor(options = {}) {
+class MakeContainer {
+  constructor(image, cmd, options = {}) {
+    this.image = image;
+    this.cmd = cmd
     this.fetch = new SocketConnection('/var/run/docker.sock');
     this.version = options.version || 'v1.41';
+
+    this.createOpts = {
+      AttachStdin: true,
+      AttachStdout: true,
+      AttachStderr: true,
+      OpenStdin: true,
+      StdinOnce: true,
+      Image: this.image,
+      ExposedPorts: {},
+      Env: [],
+      Volumes: {},
+      Cmd: cmd,
+      HostConfig: {
+        Binds: [],
+        Links: [],
+        NetworkMode: 'host',
+        PortBindings: {},
+        Privileged: true,
+      }
+    }
+
+    if (options.ports) {
+      Object.keys(opts.ports).forEach(host => {
+        const container = opts.ports[host];
+        if (!/\//.test(container)) {
+          container += '/tcp';
+        }
+        this.createOpts.ExposedPorts[container] = {};
+        this.createOpts.HostConfig.PortBindings[container] = [{ HostPort: `${host}`}];
+      });
+    }
+
+    if (options.env) {
+      Object.keys(options.env).forEach(name => {
+        this.createOpts.Env.push(`${name}=${options.env[name]}`);
+      });
+    }
+
+    if (options.volumes) {
+      for (const localName in options.volumes) {
+        let containerName = options.volumes[localName];
+        this.createOpts.Volumes[localName] = {}
+        if (!/:r(w|o)$/.test(containerName)) {
+          containerName += ':rw';
+        }
+
+        this.createOpts.HostConfig.Binds.push(`${localName}:${containerName}`);
+      }
+    }
   }
 
-  async create(image, cmd, options = {}) {
-    const createOpts = {
-      Image: image,
-      Cmd: cmd,
-    };
+  async create(options = {}) {
     const path = this.addVersion('/containers/create');
-    const response = await this.fetch.post(path, { json: createOpts });
+    const response = await this.fetch.post(path, { json: this.createOpts });
 
     return response;
   }
@@ -48,48 +97,17 @@ class DockerDaemon {
   }
 }
 
-// create container
-// request.post('/containers/create', {json: copts, qs })
-
-// attach to container
-// qs = { stderr: 1, stdout: 1, stdin: 1, stream: 1 };
-// headers = {'Content-Length': '0'};
-// request.post('/containers/<id>/attach', { qs, headers })
-
-// delete container
-// request.del('/containers/<id>')
-
-// stop container
-// qs = { opts.wait || 10 };
-// request.post('/containers/<id>/stop', {qs, json: true, body: null});
-
-// start container
-// request.post('/container/<id>/start', {json: {}})
-
-// wait for container to exit
-// request.post('/containers/<id>/wait', {json: true, body: null});
-
-// resize?
-// qs = { h: height, w: width };
-// request.post('/containers/<id>/resize', { qs, buffer: true, body: null });
-
-// sequence
-// create
-// attach
-// start
-//  ? resize
-//  stdin/stdout/stderr
-// wait
-//  ? remove
-
 
 class DockerRun {
-  constructor() {
-    this.dd = new DockerDaemon();
+  constructor(image, cmd, options = {}) {
+    this.image = image;
+    this.cmd = cmd;
+    this.dd = new MakeContainer(image, cmd, options);
   }
 
-  async run(image, cmdLine, options = {}) {
-    let { statusCode, body } = await this.dd.create(image, cmdLine);
+  async run(options = {}) {
+    debug('creating container with %s', this.image);
+    let { statusCode, body } = await this.dd.create();
     if (statusCode !== 201) {
       throw DockerRun.makeError('create', { statusCode, body })
     }
@@ -98,22 +116,29 @@ class DockerRun {
     if (body.warnings) {
       console.log('create warnings', body.warnings);
     }
+    debug('container %s created from %s', id, this.image);
 
     if (options.attach) {
+      debug('attaching to %s', this.image);
       const { req, res } = await this.dd.attach(id);
       if (res.statusCode >= 400) {
         throw DockerRun.makeError('attach', { statusCode, body })
       }
-      console.log('attach')
+      const parser = res.pipe(dockerRawStream());
+
+      req.on('finish', function() {
+        req.socket.end();
+      })
+
     }
 
-    console.log('starting');
+    debug('starting %s', this.image);
     ({ statusCode, body } = await this.dd.start(id));
     if (statusCode !== 204) {
       throw DockerRun.makeError('start', { statusCode, body })
     }
 
-    console.log('waiting');
+    debug('waiting for %s to complete', this.image);
     ({ statusCode, body } = await this.dd.wait(id));
     if (statusCode !== 200) {
       throw DockerRun.makeError('create', { statusCode, body })
@@ -131,7 +156,7 @@ class DockerRun {
 }
 
 module.exports = {
-  DockerDaemon,
+  MakeContainer,
   DockerRun,
 };
 
